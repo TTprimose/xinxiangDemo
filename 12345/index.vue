bcd0a73ce83fff522049f2cc07913915aca302d5
liyike <978795746@qq.com>
2021-04-08 10:26:36 +0800

    提交演示111

---
+++ 12345/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class=''>123212312</div>
+</template>
+
+<script>
+
+export default {
+    components: {},
+    data() {
+        return {
+
+        };
+    },
+    computed: {},
+    watch: {},
+    methods: {
+
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    activated() {} 
+}
+</script>
+
+<style scoped>
+
+</style>
